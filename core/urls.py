c18a6eb843fe0d15984b1bda262d4a6de7218f80
Gaston Franco <gastonfr24@gmail.com>
2023-01-26 18:53:16 -0300

    url flxed

--- core/urls.py
+++ core/urls.py
@@ -12,5 +12,5 @@ urlpatterns = [
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # Front
- urlpatterns += [re_path(r'^.*',
+urlpatterns += [re_path(r'^.*',
                          TemplateView.as_view(template_name='index.html'))]
