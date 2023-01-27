05d8bdd50afc9526c043d5e3a2a9d5714c1a8357
Gaston Franco <gastonfr24@gmail.com>
2023-01-27 16:39:01 -0300

    url media

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,7 @@ from django.views.generic import TemplateView
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -14,3 +15,8 @@ urlpatterns = [
 # Front
 urlpatterns += [re_path(r'^.*',
                         TemplateView.as_view(template_name='index.html'))]
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {
+        'document_root': settings.MEDIA_ROOT,
+    }),
+]
