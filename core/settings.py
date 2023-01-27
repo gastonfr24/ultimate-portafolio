4e16fbf3e7ef9ef6fdfd97cee25b15e1f5fcdb1a
Gaston Franco <gastonfr24@gmail.com>
2023-01-27 13:29:43 -0300

    debug false

--- core/settings.py
+++ core/settings.py
@@ -14,12 +14,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = [
-        'gastonfr.com',
-        '.gastonfr.com',
-        "https://gastonfr.com",
-        'https://www.gastonfr.com'
-        'www.gastonfr.com'
-    ]
+    "localhost",
+    "127.0.0.1"
+]
 
 if not DEBUG:
     ALLOWED_HOSTS = [
@@ -76,13 +73,19 @@ ROOT_URLCONF = 'core.urls'
 
 
 CORS_ORIGIN_WHITELIST = [
-        "https://gastonfr.com"
-        'https://www.gastonfr.com',
-    ]
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+    'http://127.0.0.1:8000',
+    
+
+]
+CSRF_COOKIE_DOMAIN = "gastonfr.com"
 CSRF_TRUSTED_ORIGINS = [
-        "https://gastonfr.com",
-        "https://www.gastonfr.com",
-    ]
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+    'http://127.0.0.1:8000',
+
+]
 
 if not DEBUG:
 #     CORS_ALLOWED_ORIGINS = [
