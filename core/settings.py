cbd372a4940c2667c0ba1fde02fc0211c0f11212
Gaston Franco <gastonfr24@gmail.com>
2023-01-29 12:21:53 -0300

    debug

--- core/settings.py
+++ core/settings.py
@@ -11,13 +11,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY
 SECRET_KEY = os.environ.get('SECRET_KEY')
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
         '*'
     ]
 
-if not DEBUG:
+if DEBUG:
     ALLOWED_HOSTS = [
         'gastonfr.com',
         '.gastonfr.com',
@@ -82,7 +82,7 @@ CSRF_TRUSTED_ORIGINS = [
         "http://localhost:8000"
     ]
 
-if not DEBUG:
+if DEBUG:
     CORS_ORIGIN_WHITELIST = [
         "https://gastonfr.com",
         'https://www.gastonfr.com',
@@ -209,7 +209,7 @@ EMAIL_BACKEND='django.core.mail.backends.console.EmailBackend'
 
 
 
-if not DEBUG:
+if DEBUG:
     # Email settings
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_PORT = 587
