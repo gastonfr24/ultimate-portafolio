c38b02e76f22f9fff5086669cd165659ebcbed66
Gaston Franco <gastonfr24@gmail.com>
2023-01-29 09:12:01 -0300

    contact view

--- apps/contacts/views.py
+++ apps/contacts/views.py
@@ -3,34 +3,49 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import Contact
 from django.template.loader import render_to_string
+from django.core.mail import EmailMultiAlternatives
+from django.conf import settings
+from rest_framework import permissions
 
 class ContactCreateView(APIView):
+    permission_classes = (permissions.AllowAny,)
     def post(self, request, format=None):
-        data = self.request.data
-
-        name = data['name']
-        email= data['email']
-        subject = data['subject']
-        message = data['message']
-
-        template = render_to_string('mail/correo.html', {
-                'name': name,
-                'body': message,
-                'email': email,
-                'subject':subject
-            })
+        
         try:
+            data = self.request.data
+
+            name = data['name']
+            email= data['email']
+            subject = data['subject']
+            body = data['message']
+
+            template = render_to_string('mail/correo.html', {
+                    'name': name,
+                    'body': body,
+                    'email': email,
+                    'subject':subject
+                })
+
+            email = EmailMultiAlternatives(
+                body,
+                template,
+                settings.EMAIL_HOST_USER,
+                ['gfdscience@gmail.com']
+            )
+
             email.attach_alternative(template, "text/html")
             email.fail_silently = False
             email.send()
-
+        except:
+            return Response({'success':'Error al enviar el correo'}, status = status.HTTP_200_OK)
+        try:
             Contact.objects.create(
                     name=name,
                     email=email,
                     subject=subject,
-                    message=message,
+                    message=body,
                     )
 
             return Response({'success':'Mensaje enviado correctamente'}, status = status.HTTP_200_OK)
         except:
-            return Response({'error':'El mensaje no se ha podido enviar'}, status = status.HTTP_500_INTERNAL_SERVER_ERROR)
+            return Response({'error':'Error al crear registro'}, status = status.HTTP_500_INTERNAL_SERVER_ERROR)
