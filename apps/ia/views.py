51e680ac2ec4371c5961bea990d01f3c69b8a84a
Gaston Franco <gastonfr24@gmail.com>
2023-02-15 19:44:07 -0300

    keys

--- apps/ia/views.py
+++ apps/ia/views.py
@@ -15,8 +15,8 @@ class PredictPriceView(APIView):
         try:
             # Datos de la predicción
             session = boto3.Session(
-                aws_access_key_id=settings.ACCESS_KEY_ID,
-                aws_secret_access_key=settings.SECRET_ACCESS_KEY
+                aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+                aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY
             )
 
             # Crea un objeto S3
